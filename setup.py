5dc747738ecd569dba834ae13945c142658f2613
Shoruda <axel.bergaman@gmail.com>
2025-05-05 13:52:31 +0200

    champions and macthes tables created

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def execute_query(connection, query):
 
 try:
     cnx = mysql.connector.connect(user='root',
-                                  password='',
+                                  password='tintin2014',
                                   host='localhost')
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -30,7 +30,7 @@ else:
     execute_query(cnx, "CREATE DATABASE LEC_STATS")
 
     cnx = mysql.connector.connect(user='root',
-                                  password='Liooil10!',
+                                  password='tintin2014',
                                   host='localhost',
                                   database="LEC_STATS")
 
@@ -42,7 +42,7 @@ else:
     + ");")
 
     execute_query(cnx, "CREATE TABLE Teams ("
-    + "TeamName VARCHAR(50) PRIMARY KEY NOT NULL,"
+    + "Name VARCHAR(50) PRIMARY KEY NOT NULL,"
     + "Coach VARCHAR(50),"
     + "Top VARCHAR(50),"
     + "Jungle VARCHAR(50),"
@@ -56,9 +56,40 @@ else:
 	+ "FOREIGN KEY(ADC) REFERENCES Players(Name),"
 	+ "FOREIGN KEY(Support) REFERENCES Players(Name)"
     + ");")
+    
+    execute_query(cnx, "CREATE TABLE Champions ("
+    + "Name VARCHAR(50) PRIMARY KEY NOT NULL,"
+    + "ReleaseDate VARCHAR(50)"
+    +");")
+
+    execute_query(cnx, "CREATE TABLE Matches ("
+    + "MatchID INT PRIMARY KEY NOT NULL,"
+    + "RedSideTeam VARCHAR(50),"
+    + "RedSideJgl VARCHAR(50),"
+    + "RedSideMid VARCHAR(50),"
+    + "RedSideAdc VARCHAR(50),"
+    + "RedSideSup VARCHAR(50),"
+    + "BlueSideTeam VARCHAR(50),"
+    + "BlueSideJgl VARCHAR(50),"
+    + "BlueSideMid VARCHAR(50),"
+    + "BlueSideAdc VARCHAR(50),"
+    + "BlueSideSup VARCHAR(50),"
+    + "WinningTeam VARCHAR(50),"
+    + "FOREIGN KEY(RedSideTeam) REFERENCES Teams(Name),"
+    + "FOREIGN KEY(RedSideJgl) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(RedSideMid) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(RedSideAdc) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(RedSideSup) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(BlueSideTeam) REFERENCES Teams(Name),"
+    + "FOREIGN KEY(BlueSideJgl) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(BlueSideMid) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(BlueSideAdc) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(BlueSideSup) REFERENCES Champions(Name),"
+    + "FOREIGN KEY(WinningTeam) REFERENCES Teams(Name)"
+    +");")
 
     execute_query(cnx, "ALTER TABLE Players "
         + "ADD CONSTRAINT fk1 "
 	    + "FOREIGN KEY (Team) "
-	    + "REFERENCES Teams(TeamName)")
+	    + "REFERENCES Teams(Name)")
     cnx.close()
