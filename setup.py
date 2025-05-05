faa1d5840eebf0802610cd2241b5b17adb94e0e1
Shoruda <axel.bergaman@gmail.com>
2025-05-05 11:33:03 +0200

    i fix

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #run to setup database with proper tables
 import mysql.connector
+from mysql.connector import errorcode
 
 try:
-    cnx = mysql.connector.connect(user='root', password='tintin2014', host='127.0.0.1', database='DV1663')
+    cnx = mysql.connector.connect(user='root', password='tintin2    014', host='127.0.0.1', database='DV1663')
 except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
 else:
-  cnx.close()
+    cnx.close()
